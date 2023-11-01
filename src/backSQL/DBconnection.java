a5b5c18cd87ebd45e06eaf2997e69a5f1c1f323b
Ram Dev <ramDvlp@gmail.com>
2023-11-01 11:13:16 +0200

    Execution of Select query added. Converting the ResultSet to String.

--- src/backSQL/DBconnection.java
+++ src/backSQL/DBconnection.java
@@ -4,12 +4,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 
 
 public class DBconnection {
+
+	
+	public static final  String propfileName = "db.properties";
+	public static final String defaultQuery = "SELECT first_name, last_name, email FROM candidates";
 
     /**
      * Get database connection
@@ -17,9 +23,6 @@ public class DBconnection {
      * @return a Connection object
      * @throws SQLException
      */
-	
-	public static final  String propfileName = "db.properties";
-	
     public static Connection getConnection() throws SQLException {
         Connection conn = null;
 
@@ -40,6 +43,45 @@ public class DBconnection {
             System.out.println(e.getMessage());
         }
         return conn;
+    }
+    
+    /**
+     * Running the given query (default otherwise) on the data base
+     * 
+     * @param conn
+     * @param query
+     * @return the string representation of the result set data.
+     */
+    public static String runSelectQuery(Connection conn, String query) {
+		if(query == null || query == "") {
+			query = defaultQuery;
+		}
+		StringBuffer resultData = new StringBuffer();
+		
+		try (Statement stmt = conn.createStatement()) {
+			ResultSet rs = stmt.executeQuery(query);
+			
+			/* provides information about the columns of the retrieved set, such as column count, name, data type etc.
+				System.out.println(rs.getMetaData());
+			*/
+			
+			//Retrieving data from the result set
+			
+			while (rs.next()) {
+				resultData.append(rs.getString("first_name") + "\t" + 
+				                      rs.getString("last_name")  + "\t" +
+				                      rs.getString("email") + "\t");
+				                    
+				}
+			
+			rs.close();
+			
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		}
+
+    	return resultData.toString();
+    	
     }
 
 }
--- src/backSQL/Prog.java
+++ src/backSQL/Prog.java
@@ -9,7 +9,16 @@ public class Prog {
 		// create a new connection to DB
 		try (Connection conn = DBconnection.getConnection()) {
 
-			System.out.println(String.format("Connected to database %s successfully.", conn.getCatalog()));
+			System.out.println(String.format("Connected to database %s successfully.\n", conn.getCatalog()));
+			
+			String result = DBconnection.runSelectQuery(conn, "");
+			System.out.println("\t" + result + "\n");
+			
+			//for future use, split the string to parts
+			String[] resultData = result.split("\t");
+			System.out.println(resultData[0] + " " + resultData[1] + " " + resultData[2]);
+			System.out.println(resultData[3] + " " + resultData[4] + " " + resultData[5]);
+			
 		} catch (SQLException ex) {
 			System.out.println(ex.getMessage());
 		}
