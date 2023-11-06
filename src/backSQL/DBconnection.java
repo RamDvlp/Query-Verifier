8bbfcba54abb74e773fb44c25511ba4c242584d5
Ram Dev <ramDvlp@gmail.com>
2023-11-06 18:17:20 +0200

    Update and Insert Queries added. Preparation of general query use cases.

--- src/backSQL/DBconnection.java
+++ src/backSQL/DBconnection.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -18,8 +20,12 @@ public class DBconnection {
 	private static String[] columns;
 
 	private static String[] queryParts;
+	
+	public static enum qType { SELECT, UPDATE, INSERT};
 
 	public static final String propfileName = "db.properties";
+	
+	private static qType querytype;
 
 	/**
 	 * Get database connection
@@ -67,7 +73,7 @@ public class DBconnection {
 			} else {
 				dissectQuery(conn, query);
 			}
-			dissectQuery(conn, query);
+			//dissectQuery(conn, query);
 			ResultSet rs = stmt.executeQuery(query);
 
 			/*
@@ -75,11 +81,7 @@ public class DBconnection {
 			 * count, name, data type etc. System.out.println(rs.getMetaData());
 			 */
 
-			int columnAmount = rs.getMetaData().getColumnCount();
-			columns = new String[columnAmount];
-			for (int i = 0; i < columnAmount; i++) {
-				columns[i] = rs.getMetaData().getColumnLabel(i + 1);
-			}
+			getColumnsNameOfQuery(rs);
 
 			resultData.append("\t\n");
 
@@ -102,20 +104,38 @@ public class DBconnection {
 
 	}
 
+	private static void getColumnsNameOfQuery(ResultSet rs) {
+		try {
+			int columnAmount = rs.getMetaData().getColumnCount();
+			columns = new String[columnAmount];
+			for (int i = 0; i < columnAmount; i++) {
+				columns[i] = rs.getMetaData().getColumnLabel(i + 1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
 	private static void dissectQuery(Connection conn, String query) {
 
-		boolean isSelectQuery = false;
-		ArrayList<String> listOfColumns = new ArrayList<String>();
+//		boolean isSelectQuery = false;
+//		ArrayList<String> listOfColumns = new ArrayList<String>();
 
 		queryParts = query.split("[ |\t|\n]+");
 		for (String s : queryParts) {
 			System.out.println(s);
 			if (s.equals("SELECT")) {
+				querytype = qType.SELECT;
 				// columns = Arrays.copyOfRange(queryParts, 1,
 				// Arrays.asList(queryParts).indexOf("FROM"));
 
 				// TODO figure out what to do with asterisk * as well as if it is necessary at
 				// all.
+			} else if( s.equals("INSERT")) {
+				querytype = qType.INSERT;
+			} else {
+				querytype = qType.UPDATE;
 			}
 		}
 
@@ -142,6 +162,69 @@ public class DBconnection {
 			e.printStackTrace();
 		}
 		return query;
+	}
+
+	public static int runUpdateQuery(Connection conn, String string) {
+		String sqlUpdate = "UPDATE candidates "
+                + "SET last_name = ? "
+                + "WHERE id = ?";
+
+		String lastName = "Will SEcond";
+		int id = 100;
+
+		int lineModified = 0;
+		//just like regular statement, but with variables.
+		try (PreparedStatement pstmt = conn.prepareStatement(sqlUpdate)){
+
+			// substitute the question mark ? with the value of variable
+			pstmt.setString(1, lastName);
+			pstmt.setInt(2, id);
+			
+			lineModified = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return lineModified;
+	}
+
+	public static int runInsertQuery(Connection conn, String string) {
+		String sql = "INSERT INTO candidates(first_name,last_name,dob,phone,email) "
+	            + "VALUES(?,?,?,?,?)";
+		
+		ResultSet rs = null;
+		int newID = 0;
+
+		try (PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+			String firstName = "YU";
+			// set parameters for statement
+			pstmt.setString(1, firstName);
+			String lastName = "HAN";
+			pstmt.setString(2, lastName);
+			Date dob = new Date(11,11,2011);
+			pstmt.setDate(3, dob);
+			String phone = "11111111";
+			pstmt.setString(4, phone);
+			String email = "ff@walla.com";
+			pstmt.setString(5, email);
+			
+			int rowReturned = pstmt.executeUpdate();
+			if (rowReturned == 1) {
+				rs = pstmt.getGeneratedKeys();
+				if(rs.next()) {
+					newID = rs.getInt(1);
+				}
+				
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return newID;
+
+
 	}
 
 }
--- src/backSQL/Prog.java
+++ src/backSQL/Prog.java
@@ -11,14 +11,23 @@ public class Prog {
 
 			System.out.println(String.format("Connected to database %s successfully.\n", conn.getCatalog()));
 			
-			String result = DBconnection.runSelectQuery(conn, "");
-			System.out.println("\t" + result + "\n");
+			String queryForUpdate = "SELECT * FROM candidates WHERE id = 100";
+			String resultSelect = DBconnection.runSelectQuery(conn, queryForUpdate);
+			System.out.println("\t" + resultSelect + "\n");
 			
-			//for future use, split the string to parts
-			//String[] resultData = result.split("\t");
-			//System.out.println(resultData[0] + " " + resultData[1] + " " + resultData[2]);
-			//System.out.println(resultData[3] + " " + resultData[4] + " " + resultData[5]);
+			System.out.println("\nRun update query (hardcoded variable values)");
+			System.out.println("UPDATE candidates " + 
+					"                SET last_name = ? " + 
+					"                + WHERE id = ?");
+			int resultUpdateNumLines = DBconnection.runUpdateQuery(conn, "");
 			
+			System.out.println("Number of rows affected: " + resultUpdateNumLines);
+			
+			System.out.println("\n Excecuting insert (hardcoced values)");
+			System.out.println("INSERT INTO candidates(first_name,last_name,dob,phone,email)\n VALUES(?,?,?,?,?)");
+			//int idReturned = DBconnection.runInsertQuery(conn, "");
+			//System.out.println("\n The id of new entery is: " + idReturned);
+			System.out.println(DBconnection.runSelectQuery(conn, "SELECT * FROM candidates WHERE id = 135"));
 			
 		} catch (SQLException ex) {
 			System.out.println(ex.getMessage());           
