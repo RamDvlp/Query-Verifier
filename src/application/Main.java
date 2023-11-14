add0275bfc2680a1e3be002433a63fe601711936
Ram Dev <ramDvlp@gmail.com>
2023-11-14 22:15:01 +0200

    Basic V.alpha gui made in FXML file

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,11 +13,13 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Home.fxml"));
-			Scene scene = new Scene(root,500,500);
+			Scene scene = new Scene(root,1280,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setMaximized(true);
+			//primaryStage.setMaximized(true);
+			primaryStage.setResizable(false);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
